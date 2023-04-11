62b3e2409a7b64c93727722024d75d5608c3a374
Matthew Mahoney <mmahoney@redhat.com>
2023-04-11 14:15:56 -0400

    Update integ-suite-kind.sh
    DO NOT MERGE

--- prow/integ-suite-kind.sh
+++ prow/integ-suite-kind.sh
@@ -172,6 +172,9 @@ if [[ -z "${SKIP_BUILD:-}" ]]; then
   trace "build images" build_images "${PARAMS[*]}"
 fi
 
+# MattM: Force build
+trace "build images" build_images "${PARAMS[*]}"
+
 # If a variant is defined, update the tag accordingly
 if [[ -n "${VARIANT:-}" ]]; then
   export TAG="${TAG}-${VARIANT}"
